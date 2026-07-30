c6591ab09e80f9eb68f7726a608b5d6bf1fd5378
Ashwani Kumar <ashkr2789@gmail.com>
2026-07-30 22:35:56 +0530

    initial commit

---
+++ client/src/main.jsx
@@ -0,0 +1,17 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./app/store.js";
+import { ClerkProvider } from "@clerk/react";
+import { Provider } from "react-redux";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <ClerkProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ClerkProvider>
+  </BrowserRouter>,
+);
